01185fee0dbe87c33dd57ba90051e6e981c2c6c1
Ajibade Adebayo <asqwrd@gmail.com>
2018-08-05 21:15:38 -0700

    updates to api

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get("/api/explore/continent/:continent_id", (req, res) => {
         method: "GET",
         uri: `https://api.sygictravelapi.com/1.1/en/places/list?rating=.0005:&level=country&parents=continent:${
         req.params.continent_id
-        }&limit:10`,
+        }&limit=5`,
         json: true,
         headers: { "x-api-key": "FncICCs1xe5u0DSDQCiYT3nDi8k8gaXH32xw0xjG" }
     });
@@ -110,7 +110,7 @@ app.get("/api/explore/continent/:continent_id", (req, res) => {
         method: "GET",
         uri: `https://api.sygictravelapi.com/1.1/en/places/list?&level=poi&parents=continent:${
         req.params.continent_id
-        }&limit:10`,
+        }&limit=5`,
         json: true,
         headers: { "x-api-key": "FncICCs1xe5u0DSDQCiYT3nDi8k8gaXH32xw0xjG" }
     });
@@ -119,7 +119,7 @@ app.get("/api/explore/continent/:continent_id", (req, res) => {
         method: "GET",
         uri: `https://api.sygictravelapi.com/1.1/en/places/list?rating=.0005:&level=city&parents=continent:${
             req.params.continent_id
-            }&limit:10`,
+            }&limit=5`,
         json: true,
         headers: { "x-api-key": "FncICCs1xe5u0DSDQCiYT3nDi8k8gaXH32xw0xjG" }
     });
@@ -141,69 +141,187 @@ app.get("/api/explore/continent/:continent_id", (req, res) => {
         getAllCountries
     ])
       .then(responses => {
-        const popular_countries = responses[0].data.places.reduce(
+        let popular_countries = responses[0].data.places.reduce(
           (acc, curr) => {
             return [
               ...acc,
-              {
+              `${curr.id}`
+             /* {
+                sygic_id: curr.id,
+                thumbnail_url: curr.thumbnail_url,
+                name:curr.name,
+                name_suffix:curr.name_suffix,
+                parent_ids: curr.parent_ids,
+              }*/
+            ];
+          },
+          []
+        );
+
+        let pop_countries_id = popular_countries.join('|');
+
+        let popular_countries_media = request({
+            method: "GET",
+            uri: encodeURI(`https://api.sygictravelapi.com/1.1/en/places?ids=${pop_countries_id}`),
+            json: true,
+            headers: { "x-api-key": "FncICCs1xe5u0DSDQCiYT3nDi8k8gaXH32xw0xjG" }
+        });
+        
+
+        let points_of_interest = responses[1].data.places.reduce(
+          (acc, curr) => {
+            return [
+              ...acc,
+              `${curr.id}`
+              /*{
                 sygic_id: curr.id,
                 thumbnail_url: curr.thumbnail_url,
                 name:curr.name,
                 name_suffix:curr.name_suffix,
                 parent_ids: curr.parent_ids
+              }*/
+            ];
+          },
+          []
+        );
+
+        let points_of_interest_id = points_of_interest.join('|');
+
+        let points_of_interest_media = request({
+            method: "GET",
+            uri: encodeURI(`https://api.sygictravelapi.com/1.1/en/places?ids=${points_of_interest_id}`),
+            json: true,
+            headers: { "x-api-key": "FncICCs1xe5u0DSDQCiYT3nDi8k8gaXH32xw0xjG" }
+        });
+
+        let popular_cities = responses[2].data.places.reduce(
+          (acc, curr) => {
+            return [
+              ...acc,
+              `${curr.id}`
+              /*{
+                sygic_id: curr.id,
+                thumbnail_url: curr.thumbnail_url,
+                name:curr.name,
+                name_suffix:curr.name_suffix,
+                parent_ids: curr.parent_ids
+              }*/
+            ];
+          },
+          []
+        );
+
+        let popular_cities_id = popular_cities.join('|');
+
+        let popular_cities_media = request({
+            method: "GET",
+            uri: encodeURI(`https://api.sygictravelapi.com/1.1/en/places?ids=${popular_cities_id}`),
+            json: true,
+            headers: { "x-api-key": "FncICCs1xe5u0DSDQCiYT3nDi8k8gaXH32xw0xjG" }
+        });
+
+        let all_countries = responses[3].data.places.reduce((acc, curr) => {
+          return [
+              ...acc,
+              `${curr.id}`,
+             /* {
+                sygic_id: curr.id,
+                thumbnail_url: curr.thumbnail_url,
+                name:curr.name,
+                name_suffix:curr.name_suffix,
+                parent_ids: curr.parent_ids
+              }*/
+            ];
+        },[]);
+
+        let all_countries_id = all_countries.join('|');
+
+        let all_countries_media = request({
+            method: "GET",
+            uri: encodeURI(`https://api.sygictravelapi.com/1.1/en/places?ids=${all_countries_id}`),
+            json: true,
+            headers: { "x-api-key": "FncICCs1xe5u0DSDQCiYT3nDi8k8gaXH32xw0xjG" }
+        });
+
+
+        return Promise.all([popular_countries_media, points_of_interest_media, popular_cities_media, all_countries_media]) 
+      })
+      .then(([popular_countries_res, points_of_interest_res, popular_cities_res, all_countries_res])=>{
+        const popular_countries = popular_countries_res.data.places.reduce(
+          (acc, curr) => {
+            return [
+              ...acc,
+              {
+                sygic_id: curr.id,
+                image: curr.main_media ? curr.main_media.media[0].url : curr.main_media,
+                name:curr.name,
+                name_suffix:curr.name_suffix,
+                parent_ids: curr.parent_ids,
+                description: curr.perex
               }
             ];
           },
           []
         );
 
-        const points_of_interest = responses[1].data.places.reduce(
-            (acc, curr) => {
-              return [
-                ...acc,
-                {
-                  sygic_id: curr.id,
-                  thumbnail_url: curr.thumbnail_url,
-                  name:curr.name,
-                  name_suffix:curr.name_suffix,
-                  parent_ids: curr.parent_ids
-                }
-              ];
-            },
-            []
-          );
 
-          const popular_cities = responses[2].data.places.reduce(
-            (acc, curr) => {
-              return [
-                ...acc,
-                {
-                  sygic_id: curr.id,
-                  thumbnail_url: curr.thumbnail_url,
-                  name:curr.name,
-                  name_suffix:curr.name_suffix,
-                  parent_ids: curr.parent_ids
-                }
-              ];
-            },
-            []
-          );
-
-        const all_countries = responses[3].data.places.reduce((acc, curr) => {
+        const popular_cities = popular_cities_res.data.places.reduce(
+          (acc, curr) => {
             return [
-                ...acc,
-                {
-                  sygic_id: curr.id,
-                  thumbnail_url: curr.thumbnail_url,
-                  name:curr.name,
-                  name_suffix:curr.name_suffix,
-                  parent_ids: curr.parent_ids
-                }
-              ];
-            },[]);
+              ...acc,
+              {
+                sygic_id: curr.id,
+                image: curr.main_media ? curr.main_media.media[0].url : curr.main_media,
+                name:curr.name,
+                name_suffix:curr.name_suffix,
+                parent_ids: curr.parent_ids,
+                description: curr.perex
+              }
+            ];
+          },
+          []
+        );
+        
+        const points_of_interest = points_of_interest_res.data.places.reduce(
+          (acc, curr) => {
+            return [
+              ...acc,
+              {
+                sygic_id: curr.id,
+                image: curr.main_media ? curr.main_media.media[0].url : curr.main_media,
+                name:curr.name,
+                name_suffix:curr.name_suffix,
+                parent_ids: curr.parent_ids,
+                description: curr.perex
+              }
+            ];
+          },
+          []
+        );
+
+        console.log(points_of_interest.length);
+
+        const all_countries = all_countries_res.data.places.reduce(
+          (acc, curr) => {
+            return [
+              ...acc,
+              {
+                sygic_id: curr.id,
+                image: curr.main_media ? curr.main_media.media[0].url : curr.main_media,
+                name:curr.name,
+                name_suffix:curr.name_suffix,
+                parent_ids: curr.parent_ids,
+                description: curr.perex
+              }
+            ];
+          },
+          []
+        );
+        console.log(popular_countries.length);
         res.send({ popular_countries, popular_cities, points_of_interest, all_countries });
       })
       .catch(err => {
+        console.log(err);
         res.send(err);
       });
 });
